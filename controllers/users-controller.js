4de4b1a903db75e9d953d556150bbcb215d97a8a
Fikret OZTURK <vbdeveloper@windowslive.com>
2020-05-24 23:42:34 +0300

    Removes unused variable assignment

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -25,7 +25,7 @@ const singUp = async (req, res, next) => {
     );
   }
 
-  const { name, email, password, image } = req.body;
+  const { name, email, password } = req.body;
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email });
@@ -153,7 +153,7 @@ const login = async (req, res, next) => {
     message: "Logged in!",
     userId: identifiedUser.id,
     email: identifiedUser.email,
-    token: token
+    token: token,
   });
 };
 
